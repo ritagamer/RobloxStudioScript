6bf8f05dc04e1ff997f70ec59a71d9d681123202
RobloxStudioTutorial <110125027+ritagamer@users.noreply.github.com>
2022-07-27 22:13:43 +0200

    Create script_2.lua
    02 CONDICIONALES CURSO DE SCRIPTING ROBLOX STUDIO #2

---
+++ script_2.lua
@@ -0,0 +1,24 @@
+-- CONDICIONALES 
+
+-- EJEMPLO 1 - Programa de mayor de edad
+
+valor_usuario = 16
+
+if valor_usuario >= 18 then
+    print("Si eres mayor de edad")
+else
+    print("No eres mayor de edad")
+end
+
+-- EJEMPLO 2 - Organizar numeros mayor o menor
+
+numero1 = 14
+numero2 = 
+
+if numero1 < numero2 then
+    print(numero1, "es menor que", numero2)
+elseif numero1 > numero2 then
+    print(numero2, "es mayor que", numero1)
+elseif numero1 == numero2 then
+    print(numero1, "y", numero2, "son iguales")
+end
